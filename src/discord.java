6f379962d4138cefdd639e96c12c4c695ec37464
DAM_1 <DAM_1@PB06>
2019-12-13 19:44:28 +0100

    resta

--- src/discord.java
+++ src/discord.java
@@ -8,6 +8,14 @@ public class discord {
 		return total;
 	}
 	
+	public static int resta ( int operando1,int operando2) {
+		int total = 0;
+		total = operando1 - operando2;
+		return total;
+	}
+	
+
+	
 	
 	
 	public static int leerNumero (String Cadena) {
